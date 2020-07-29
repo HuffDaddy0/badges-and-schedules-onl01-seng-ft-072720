7973b9959b5bef6f5e26ee0a2c9f61213b3d8e31
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 13:40:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,14 @@ end
 
 def batch_badge_creator(array)
   badge_array = []
-  array.each |name| {badge_array.push(badge_maker(name))}
+  array.each |name| do
+    badge_array.push(badge_maker(name))
+  end
   badge_array
 end
 
-
+def assign_rooms
+end
 
 
 # Write your code here.
