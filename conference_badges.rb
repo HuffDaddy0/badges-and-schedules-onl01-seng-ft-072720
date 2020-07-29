37f59692d42920dc3a316b08ad0ae23c68c1c2a9
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 13:37:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,7 @@ end
 
 def batch_badge_creator(array)
   badge_array = []
-  array.each |name| {}
-    badge_array.push(badge_maker(name))
-  end
+  array.each |name| {badge_array.push(badge_maker(name))}
   badge_array
 end
 
