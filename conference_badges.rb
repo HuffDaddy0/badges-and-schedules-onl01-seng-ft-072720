6cbc951e90698b241bd7ab71f994078553ed62db
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 13:34:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each |name| do
+    badge_array << badge_maker(name)
+  end
+end
+
+
+
+
 # Write your code here.
